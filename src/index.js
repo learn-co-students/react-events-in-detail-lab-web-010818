c7f3bd850ad0ec7255c5036fbd09dc5d654dc71d
morgannegagne <morgannegagne@gmail.com>
2018-03-11 12:11:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -10,5 +11,5 @@ ReactDOM.render(
     <CoordinatesButton />
     <DelayedButton />
   </div>,
-  document.getElementById('global')
+  document.getElementById('root')
 );
